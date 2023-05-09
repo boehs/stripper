96fe3976b13242063796a651778ed68df147aeb9
Evan Boehs <evan@boehs.org>
2023-05-09 14:44:15 -0400

    this works but I don't like it

--- examples/wasm/src/gradient.rs
+++ examples/wasm/src/gradient.rs
@@ -1,5 +1,7 @@
 use stripper::{
-    primitives::color::{FromColor, Hsla, Rgba, Srgba, LinSrgba, IntoColor, rgb::Rgb, Hsl, Srgb},
+    primitives::color::{
+        rgb::Rgb, Alpha, FromColor, Hsl, Hsla, IntoColor, LinSrgba, Rgba, Srgb, Srgba,
+    },
     ModR, Module, Pixels,
 };
 
@@ -15,12 +17,11 @@ impl Module for Rainbow {
 
     fn render(&mut self, i: u32, pixels: &Pixels) -> ModR {
         let steps = 360.0 / pixels.len() as f32;
-        web_sys::console::log_1(&steps.into());
         let grad = (0..pixels.len())
             .map(|x| {
-                let c = Hsl::new((steps * x as f32) as f32, 1.0, 0.5);
-                let b = Rgba::from_color(c).into_format();
-                b
+                let c = Hsl::new_srgb(((steps * x as f32) + i as f32) % 360.0, 1.0, 0.5);
+                let b: Alpha<Rgb, f32> = Rgba::from_color(c).into_format();
+                Srgba::new(b.red * 255.0, b.green * 255.0, b.blue * 255.0, b.alpha)
             })
             .collect();
         ModR::Pixels(grad)
