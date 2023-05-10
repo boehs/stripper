24a9d56dc34a49829cbafa005f7ea5dcc42e6bb5
Evan Boehs <evan@boehs.org>
2023-05-10 09:06:38 -0400

    prog

--- examples/wasm/src/lib.rs
+++ examples/wasm/src/lib.rs
@@ -1,16 +1,26 @@
-mod weather;
 mod gradient;
+mod weather;
 
-use wasm_bindgen::prelude::wasm_bindgen;
-use stripper::{runtime::{self, wasm::WasmInit, Runtime}, Module};
+use stripper::{
+    runtime::{self, wasm::WasmInit, Runtime},
+    Module,
+};
+use wasm_bindgen::{prelude::wasm_bindgen, JsCast};
 use weather::WeatherD;
 
 #[wasm_bindgen(start)]
 fn main() {
-    runtime::common::run(
-        Box::new(gradient::Gradient::update("#FFD700,#FF69B4,#00CED1".to_string())),
-        Box::new(runtime::wasm::Wasm::new(WasmInit {
-            selector: ".pix".to_string(),
-        })),
-    )
+    let window = web_sys::window().expect("no global `window` exists");
+    let params =
+        web_sys::UrlSearchParams::new_with_str_sequence_sequence(&window.location().to_string())
+            .expect("Failed parsing params");
+
+    if let Some(imp) = params.get("imp") {
+        runtime::common::run(
+            Box::new(WeatherD::update(imp.clone())),
+            Box::new(runtime::wasm::Wasm::new(WasmInit {
+                selector: ".pix".to_string(),
+            })),
+        )
+    }
 }
