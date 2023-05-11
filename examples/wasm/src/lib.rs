272cc13ebfc43d6f65c344581fea936bf97e94aa
Evan Boehs <evan@boehs.org>
2023-05-11 00:14:41 -0400

    closes #3

--- examples/wasm/src/lib.rs
+++ examples/wasm/src/lib.rs
@@ -1,26 +1,43 @@
 mod gradient;
 mod weather;
 
+use std::rc::Rc;
+
 use stripper::{
     runtime::{self, wasm::WasmInit, Runtime},
     Module,
 };
-use wasm_bindgen::{prelude::wasm_bindgen, JsCast};
-use weather::WeatherD;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen(start)]
 fn main() {
     let window = web_sys::window().expect("no global `window` exists");
-    let params =
-        web_sys::UrlSearchParams::new_with_str_sequence_sequence(&window.location().to_string())
-            .expect("Failed parsing params");
+    let params = web_sys::UrlSearchParams::new_with_str_sequence_sequence(
+        &window
+            .location()
+            .search()
+            .expect("No search")
+            .to_string()
+            .into(),
+    )
+    .expect("Failed parsing params");
 
     if let Some(imp) = params.get("imp") {
-        runtime::common::run(
-            Box::new(gradient::Gradient::update(imp.clone())),
-            Box::new(runtime::wasm::Wasm::new(WasmInit {
-                selector: ".pix".to_string(),
-            })),
-        )
+        if let Some(idx) = params.get("idx") {
+            if let Ok(i) = usize::from_str_radix(&idx, 10) {
+                let v: Vec<Rc<dyn Module>> = vec![
+                    Rc::new(gradient::Gradient::update(imp.clone())),
+                    Rc::new(weather::WeatherD::update(imp.clone())),
+                ];
+                if let Some(modt) = v.get(i) {
+                    runtime::common::run(
+                        modt.clone(),
+                        Box::new(runtime::wasm::Wasm::new(WasmInit {
+                            selector: ".pix".to_string(),
+                        })),
+                    )
+                }
+            }
+        }
     }
 }
--- src/runtime/common.rs
+++ src/runtime/common.rs
@@ -1,3 +1,5 @@
+use std::{rc::Rc, sync::Arc};
+
 use palette::rgb::Rgba;
 use wasm_bindgen::{prelude::*, JsCast};
 
@@ -6,16 +8,18 @@ use crate::{ModR, Module, Pixels};
 use super::Runtime;
 
 // Obviously eventually this will not be browser based.
-pub fn run<T: 'static>(mut modules: Box<dyn Module>, runtime: Box<dyn Runtime<T>>) {
-    let pixels: Pixels =
-        vec![Rgba::new(0.0, 0.0, 0.0, 0.0); runtime.get_number_of_pixels().into()];
+pub fn run<T: 'static>(mut modules: Rc<dyn Module>, runtime: Box<dyn Runtime<T>>) {
+    let pixels: Pixels = vec![Rgba::new(0.0, 0.0, 0.0, 0.0); runtime.get_number_of_pixels().into()];
     runtime.display(&pixels);
 
     let mut i = 0;
     let window = web_sys::window().unwrap();
 
     let callback = Closure::wrap(Box::new(move || {
-        if let ModR::Pixels(pix) = modules.as_mut().render(i, &pixels) {
+        if let ModR::Pixels(pix) = Rc::<(dyn Module + 'static)>::get_mut(&mut modules)
+            .unwrap()
+            .render(i, &pixels)
+        {
             runtime.display(&pix);
         }
         i = i + 1
